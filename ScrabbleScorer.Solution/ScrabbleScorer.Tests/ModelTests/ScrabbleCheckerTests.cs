125748c3ec4abb6a07b1f0117f8afec12e1ab8e1
Patrick Dolan <dolanp1992@gmail.com>
2022-02-07 19:16:08 -0800

    add ScrabbleChecker constructor and prototype
    Branden Clauson: Co-authored-by: Branden Clauson <clauson.branden@gmail.com>
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>

--- ScrabbleScorer.Solution/ScrabbleScorer.Tests/ModelTests/ScrabbleCheckerTests.cs
+++ ScrabbleScorer.Solution/ScrabbleScorer.Tests/ModelTests/ScrabbleCheckerTests.cs
@@ -6,7 +6,19 @@ namespace ScrabbleScorer.Tests
   [TestClass]
   public class ScrabbleCheckerTests
   {
-    // Test methods go here
+    [TestMethod]
+    public void ScrabbleCheckerConstructor_CreateScrabbleCheckerObject_ScrabbleChecker()
+    {
+      ScrabbleChecker newScrabbleChecker = new ScrabbleChecker("Word");
+      Assert.AreEqual(typeof(ScrabbleChecker), newScrabbleChecker.GetType());
+    }
+    [TestMethod]
+    public void ScrabbleChecker_AssignWordToScrabbleChecker_ScrabbleChecker()
+    {
+      ScrabbleChecker newScrabbleChecker = new ScrabbleChecker("Word");
+      string result = newScrabbleChecker.Word;
+      Assert.AreEqual("Word", result);
+    }
   }
 }
 // Example Test Method Structure
--- ScrabbleScorer.Solution/ScrabbleScorer/Models/ScrabbleChecker.cs
+++ ScrabbleScorer.Solution/ScrabbleScorer/Models/ScrabbleChecker.cs
@@ -2,6 +2,11 @@ namespace ScrabbleScorer.Models
 {
   public class ScrabbleChecker
   {
-    
+    public string Word { get;set; }
+
+    public ScrabbleChecker(string word)
+    {
+      Word = word;
+    }
   }
 }
