4ae8f1985fd27f3f0c19bc01508dfcd394dc621c
Patrick Dolan <dolanp1992@gmail.com>
2022-02-07 20:16:20 -0800

    add recursive method to check multiple words in a row
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>

--- ScrabbleScorer.Solution/ScrabbleScorer/Program.cs
+++ ScrabbleScorer.Solution/ScrabbleScorer/Program.cs
@@ -8,10 +8,25 @@ namespace ScrabbleScorer
     public static void Main()
     {
       Console.WriteLine("Welcome to the Scrabble Scorer!");
+      RunChecker(); //We are recursive legends
+    }
+    public static void RunChecker()
+    {
       Console.WriteLine("===============================");
       Console.WriteLine("Type in a word to have it scored:");
       ScrabbleChecker scrabbleWord = new ScrabbleChecker(Console.ReadLine());
-      Console.WriteLine("Your word would score: " + scrabbleWord.CalculateScore());
+      Console.WriteLine(scrabbleWord.Word + " would score: " + scrabbleWord.CalculateScore());
+      
+      Console.WriteLine("Would you like to score another word?");
+      string checkMoreWords = Console.ReadLine();
+      if (checkMoreWords == "Y" || checkMoreWords == "y")
+      {
+        RunChecker();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye.");
+      }
     }
   }
 }
